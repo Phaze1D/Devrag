7581f074673e7af7af7379703697a1470604ac9d
DavidMBP <steadypathapp@gmail.com>
2015-07-12 12:53:29 -0500

    Adde animation for hidden drop down

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,6 +3,18 @@
  * Handles the down arror click animation
  */
 $(document).on('click','#down-arror', function(){
-
+    $(this).fadeOut('fast');
+    $('#name-title').removeClass('title-2-dead').addClass('title-2-alive underline-alive');
+    $('#name-hidden-box').animate({
+        'margin-top': '100px'
+    },'fast','linear', function () {
+        $('#name-hidden-box').fadeOut('fast');
+    });
 });
 
+
+
+/*
+    Make function for name animation show and remove, and other function for
+    LPU animation show and remove (DRY clean code)
+ */
