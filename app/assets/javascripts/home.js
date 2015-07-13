db0b800e2608ab637e0dbf8d24edc44f6b8c1063
DavidMBP <steadypathapp@gmail.com>
2015-07-12 22:38:30 -0500

    Refactor some code

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,12 +4,7 @@
  */
 $(document).on('click','#down-arror', function(){
     $(this).fadeOut('fast');
-    $('#name-title').removeClass('title-2-dead').addClass('title-2-alive underline-alive');
-    $('#name-hidden-box').animate({
-        'margin-top': '100px'
-    },'fast','linear', function () {
-        $('#name-hidden-box').fadeOut('fast');
-    });
+    openName();
 });
 
 
@@ -18,3 +13,24 @@ $(document).on('click','#down-arror', function(){
     Make function for name animation show and remove, and other function for
     LPU animation show and remove (DRY clean code)
  */
+
+function openName(){
+    $('#name-title').removeClass('title-2-dead').addClass('title-2-alive underline-alive');
+    $('#name-hidden-box').animate({
+        'margin-top': '100px'
+    },400,'linear', function () {
+        $('#name-hidden-box').fadeOut('fast');
+    });
+}
+
+function closeName(){
+
+}
+
+function openLPU(){
+
+}
+
+function closeLPU(){
+
+}
