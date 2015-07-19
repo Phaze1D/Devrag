1cde36b84cf709f315b93e9e504dafcc9fbb224e
DavidMBP <steadypathapp@gmail.com>
2015-07-18 20:46:44 -0500

    Added the main div for dropdown menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,28 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).on('click', '#header-div',function(){
+    menuAnim();
+});
+
+function menuAnim(){
+    var menu = $('#menu-div');
+
+    if(menu.css('opacity') == '1'){
+        menu.css('opacity', '0');
+        menu.css('-webkit-transform:', 'scale(.5)');
+        menu.css('-moz-transform', 'scale(.5)');
+        menu.css('-o-transform', 'scale(.5)');
+        menu.css('transform', 'scale(.5)');
+
+
+    }else{
+        menu.css('opacity', '1');
+        menu.css('-webkit-transform:', 'scale(1)');
+        menu.css('-moz-transform', 'scale(1)');
+        menu.css('-o-transform', 'scale(1)');
+        menu.css('transform', 'scale(1)');
+    }
+}
