b4e9588cf04e855570bffe64a00f0d111920435e
DavidMBP <steadypathapp@gmail.com>
2015-07-19 22:27:55 -0500

    Added the test input fields for the menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,17 +25,17 @@ function menuAnim(){
 
     if(menu.css('opacity') == '1'){
         menu.css('opacity', '0');
-        menu.css('-webkit-transform:', 'scale(.5)');
-        menu.css('-moz-transform', 'scale(.5)');
-        menu.css('-o-transform', 'scale(.5)');
-        menu.css('transform', 'scale(.5)');
+        //menu.css('-webkit-transform:', 'scale(.5)');
+        //menu.css('-moz-transform', 'scale(.5)');
+        //menu.css('-o-transform', 'scale(.5)');
+        //menu.css('transform', 'scale(.5)');
 
 
     }else{
         menu.css('opacity', '1');
-        menu.css('-webkit-transform:', 'scale(1)');
-        menu.css('-moz-transform', 'scale(1)');
-        menu.css('-o-transform', 'scale(1)');
-        menu.css('transform', 'scale(1)');
+        //menu.css('-webkit-transform:', 'scale(1)');
+        //menu.css('-moz-transform', 'scale(1)');
+        //menu.css('-o-transform', 'scale(1)');
+        //menu.css('transform', 'scale(1)');
     }
 }
