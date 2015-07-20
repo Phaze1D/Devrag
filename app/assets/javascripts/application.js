67930d7e88a2c0174e034f4bad45e50fea5b4efd
DavidMBP <steadypathapp@gmail.com>
2015-07-20 15:17:22 -0500

    Added hide action when click is outside menu-div

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,15 @@ $(document).on('click', '#header-div',function(){
     menuAnim();
 });
 
+$(document).mouseup(function (e)
+{
+    var container = $("#menu-div");
+
+    if (!container.is(e.target) && container.has(e.target).length === 0){
+        container.css('opacity', '0');
+    }
+});
+
 function menuAnim(){
     var menu = $('#menu-div');
 
