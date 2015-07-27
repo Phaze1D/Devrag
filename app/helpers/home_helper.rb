4cf812c4823e69bf20cce68a604cc29fe69c79c3
DavidMBP <steadypathapp@gmail.com>
2015-07-27 16:56:35 -0500

    Start work on the tool element result

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,14 @@
 module HomeHelper
 
+  # Display the list of search results
   def display_results
+
     html = ''
+
     @results.each do | result |
       html +=  render partial: 'home/tool_result', locals: {tool: result}
     end
-      html
+
+    html
   end
 end
