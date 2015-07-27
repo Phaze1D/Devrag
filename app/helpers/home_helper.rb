f51edc73332ce9478d49d9504e2ec99802515e06
DavidMBP <steadypathapp@gmail.com>
2015-07-26 19:39:45 -0500

    Create first draft for the tool_result

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+
+  def display_results
+    html = ''
+    @results.each do | result |
+      html +=  render partial: 'home/tool_result', locals: {tool: result}
+    end
+      html
+  end
 end
