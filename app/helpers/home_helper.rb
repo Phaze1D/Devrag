b781651b21830bf06202f7cc08d2dfdbec71caaf
DavidMBP <steadypathapp@gmail.com>
2015-07-31 18:54:47 -0500

    Added method 'resize_div' to home helper
    resize_div returns the scss id when the user
    is logged in

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,13 +2,18 @@ module HomeHelper
 
   # Display the list of search results
   def display_results
-
     html = ''
-
-    @results.each do | result |
-      html +=  render partial: 'home/tool_result', locals: {tool: result}
+    @results.each do |result|
+      html += render partial: 'home/tool_result', locals: {tool: result}
     end
-
     html
+  end
+
+  def resize_div
+    if false
+      'username-div'
+    else
+      ''
+    end
   end
 end
