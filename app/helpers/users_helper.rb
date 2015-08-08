8ea3aa2e7fa063a1f15fb722235940b0cc765838
DavidMBP <steadypathapp@gmail.com>
2015-08-07 22:09:45 -0500

    Refactor by adding methodes
    Added fake 'is_logged_in' method in user_helper.rb and
    added 'is_viewing_own_profile' method and move the Devrag <h1> label title to
    user/show view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  # Returns bool if user is view his/her profile
+  # Call in _header view to remove username if true
+  def is_viewing_own_profile?
+    controller_name == 'users' && action_name == 'show' # && is_users_profile
+  end
+
+  # Returns bool if user is logged in
+  def is_logged_in?
+    true
+  end
+
 end
