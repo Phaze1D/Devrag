bd72b53e6481a35c671c54c026c3a555512a2269
DavidMBP <steadypathapp@gmail.com>
2015-08-29 13:20:31 -0500

    Added styles and a partial variable
    Added styles and a partial variable to tools controller

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -3,6 +3,9 @@ class ToolsController < ApplicationController
 
 	def show
 		@tool = 1
+		@tools_lpu = %w(c++ short thisisalongin sja;lfadkjisalongerone    
+		thisisthelongestoneofthemalljustincaseyahearmeboy) # Change to @tool.languages or platforms or uses 
+		
 	end
 
 	def new 
