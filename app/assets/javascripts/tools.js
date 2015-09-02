d11e2ffc67da02d8dc7d1bc284a0e993760d693a
DavidMBP <steadypathapp@gmail.com>
2015-09-01 21:52:14 -0500

    Added 'Add comment section'

--- app/assets/javascripts/tools.js
+++ app/assets/javascripts/tools.js
@@ -1,11 +1,48 @@
 
 
+
+$(document).on('click', '#comment-addo', function(){
+	showAddComment();
+});
+
+$(document).on('click', '#comment-addr', function(){
+	cancelComment();
+});
+
+$(document).on('click', '#comment-addc', function(){
+	addComment();
+});
+
+
+function showAddComment(){
+	$('#comment-addo').css('display', 'none');
+	$("#add-comment-section").animate({
+		marginTop: '0px'}
+	);
+}
+
+function closeAddComment(){
+	$("#add-comment-section").animate({
+		marginTop: '-170px'}, function(){
+			$('#comment-addo').css('display', 'block');
+		});
+}
+
+function cancelComment(){
+	closeAddComment();
+}
+
+function addComment(){
+	closeAddComment();
+}
+
 function initToolLPUScroller(){
 	$('.rain-scroll').mCustomScrollbar({
         axis:"x",
         scrollbarPosition: "outside",
         autoHideScrollbar: true,
         theme: "dark-thin",
+        alwaysShowScrollbar: 0,
         scrollInertia: 0
     });
 
