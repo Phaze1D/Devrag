c5f9fc1ab92a3b5edf16a13b6925823e9ee2b1c8
DavidMBP <steadypathapp@gmail.com>
2015-09-16 22:14:13 -0500

    Added notification paritals

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -7,15 +7,15 @@ module NotificationsHelper
         case notification
 
           when 'like'
-
+            html += render(partial: 'notifications/like_noti', locals: {notification: notification})
           when 'follow'
-
+            html += render(partial: 'notifications/follow_noti', locals: {notification: notification})
           when 'commentT'
-
+            html += render(partial: 'notifications/comment_noti', locals: {notification: notification})
           when 'commentR'
-
+            html += render(partial: 'notifications/comment_noti', locals: {notification: notification})
           when 'notit'
-
+            html += render(partial: 'notifications/tool_noti', locals: {notification: notification})
           else
             # type code here
         end
