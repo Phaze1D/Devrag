8774e6f6d35573bbc5dfb6dcc9555d4edba9adff
DavidMBP <steadypathapp@gmail.com>
2015-09-19 20:19:54 -0500

    Added the noti tool and its icon

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,7 @@
 class NotificationsController < ApplicationController
 
   def index
-    @current_user_notifications = %w(like follow follow follow like commentT commentT commentT commentT
-                                      commentR commentR notit notit notit notit like like follow)
+    @current_user_notifications = %w(like follow follow like commentT notit commentT commentR notit)
     respond_to do |format|
       format.js
     end
