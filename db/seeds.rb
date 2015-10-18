7a65dec75e5e918831e9a92c7bb95e9f56b1a5ab
DavidMBP <steadypathapp@gmail.com>
2015-10-18 18:01:17 -0500

    small comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Users
+# Tools
+# Notifications
+# Relationships (Followers, Follows)
+# Comments
+# Languages
+# Platforms
+# Uses
