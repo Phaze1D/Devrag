0ae0eae15784b647d9e7e45b63da059b5853e699
DavidMBP <steadypathapp@gmail.com>
2015-10-19 20:38:57 -0500

    Starting modeling the models

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,36 @@
 
 
 # Users
+#   username, email, password, picture, activation_token, reset_token, activated, reset_sent_at
 # Tools
-# Notifications
+#   name, website, description, user_id, (likes_amount, follows_amounts, comments_amounts)
+# Reports
+#   description, for_tool_id, by_user_id
 # Relationships (Followers, Follows)
+#   id, user_id, tool_id
 # Comments
+#   comment, made_by_user_id, on_tool_id, reply_to_user_id
 # Languages
+#   name
 # Platforms
+#   name
 # Uses
+#   name
+# Languages_Tools
+#     language_id, tool_id
+# Platforms_Tools
+#     platform_id, tool_id
+# Uses_Tools
+#     uses_id, tool_id
+
+# Like_Notifications
+#   info, seen, made_by_user_id, for_tool_id
+
+# Follows_Notifications
+#   info, seen, made_by_user_id, for_tool_id
+
+# Comments_Notifications
+#   info, seen, made_by_user_id, for_tool_id, comment_id
+
+# Tool_Notifications
+#   info, seen, made_by_tool_id, for_followers_id
