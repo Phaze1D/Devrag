c659b77726b90c60f654fb26132739d3f087b236
DavidMBP <steadypathapp@gmail.com>
2015-11-01 14:23:36 -0600

    Fixed spelling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :tool
   belongs_to :reply, :class_name => 'Comment'
-  has_many :replys , :class_name => 'Comment'
+  has_many :replies , :class_name => 'Comment'
 
 end
