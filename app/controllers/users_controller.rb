380daf7595fd57952592e89bfccbefa77447856b
DavidMBP <steadypathapp@gmail.com>
2015-11-03 20:35:13 -0600

    start users profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
   def show
-
+    @user = 'user'
+    @user_tools_length = 5
+    @user_follows_length = 4
   end
 
   def new
--- app/helpers/follows_helper.rb
+++ app/helpers/follows_helper.rb
@@ -1,2 +1,13 @@
 module FollowsHelper
+
+
+  def display_user_follows(user_following_tools)
+    html = ''
+    tool_owner = 'test'
+    user_following_tools.each do |tool|
+      html += render partial: 'follows/user_following_tool', locals: {tool: tool, user: tool_owner}
+    end
+    html
+  end
+
 end
