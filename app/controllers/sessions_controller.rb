cdbded6f15eb1dd2c65b5a24801bd6fca43aaa6b
DavidMBP <steadypathapp@gmail.com>
2015-11-14 15:17:18 -0600

    start the session page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Incorrect email or password combination'
+      render 'new'
+    end
   end
 
   def destroy
