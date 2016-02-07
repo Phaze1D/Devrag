820bb1dc390277db41dba6eed1aeab611e0c1a85
DavidMBP <steadypathapp@gmail.com>
2016-02-06 18:54:51 -0600

    Added validation to reset password

---
+++ app/assets/javascripts/reset_passwords.js
@@ -0,0 +1,23 @@
+
+
+$(document).on('focusout', '.reset-input', function(){
+    ajaxValidation($('#reset-form'), $(this), ajaxUserSuccess, ajaxResetFailed);
+});
+
+
+function ajaxResetFailed(input_selector, data){
+    var error_div = input_selector.closest('.error-area').find('.error-div');
+    var name = input_selector.attr('name').replace('reset_passwords[', '').replace(']','');
+
+    if(data[name]){
+        error_div.css('display', 'block');
+        var error_ul = error_div.find('ul');
+        error_ul.html('');
+        for(var i = 0; i < data[name].length; i++){
+            error_ul.append('<li>' + data[name][i] + '</li>');
+        }
+    }else{
+        error_div.css('display', 'none');
+    }
+
+}
--- app/controllers/reset_passwords_controller.rb
+++ app/controllers/reset_passwords_controller.rb
@@ -5,11 +5,34 @@ class ResetPasswordsController < ApplicationController
 
   def new
 
+    @user = User.new
+
   end
 
   def create
-    
 
+    @user = User.find_by('email = ? OR username = ?', params[:reset_passwords][:identity], params[:reset_passwords][:identity])
+
+    respond_to do | format |
+      if @user
+
+        format.html do
+          
+        end
+
+        format.json { render json: {:success => 'true'}.to_json }
+      else
+
+        format.html do
+          @user = User.new
+          @user.errors.add(:identity, 'Email or Username does not exist')
+          render 'new'
+        end
+
+        format.json {render json: {identity: ['Email or Username does not exist']}, status: :unprocessable_entity }
+
+      end
+    end
   end
 
   def edit
