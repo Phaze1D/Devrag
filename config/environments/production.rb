5ff0c9485441f9aa39acb7c145889d88a889b6e5
DavidMBP <steadypathapp@gmail.com>
2016-02-22 14:06:51 -0600

    error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_controller.asset_host = ENV['CLOUDFRONT_URL']
+  
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
