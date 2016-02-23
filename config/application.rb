dd4abc9f171442ee50a678ba29ce4af5130fd612
DavidMBP <steadypathapp@gmail.com>
2016-02-23 15:13:39 -0600

    trying to fix fonts

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module DevragWeb
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.tinymce.install = :compile
-    config.action_dispatch.default_headers.merge!('Access-Control-Allow-Origin' => 'http://devrag.io')
+    config.action_dispatch.default_headers.merge!('Access-Control-Allow-Origin' => '*')
   end
 end
