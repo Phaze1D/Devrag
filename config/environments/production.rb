9b14786dd15b20b9240e935c941164c7145b3b6b
DavidMBP <steadypathapp@gmail.com>
2016-02-23 21:52:34 -0600

    testing http in email image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_controller.asset_host = 'https://devrag.io'
-    config.action_mailer.asset_host = 'https://devrag.io'
+    config.action_mailer.asset_host = 'http://devrag.io'
 
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
